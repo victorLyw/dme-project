f4af0281b047a9bb734a903bc55d9c78add2dd64
victorlyw <victorlyw@outlook.com>
2018-10-24 17:02:33 +0800

    config update

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -5,7 +5,7 @@
 let WEB_DOMAIN = '' // 主域名
 let PRIVATEKEY = '' // 与服务器约定的Privatekey 用于生成sign
 let AUTHOR_API = '' // 鉴权
-let WX_API = 'api' // 微信api
+let WX_API = '' // 微信api
 let APP_ID = '' // 微信appID
 let SNS_SCOPE = '' // 微信auth2.0 scope参数
 let COOKIE_NAME = {
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV == 'development') { // 测试环境
   WEB_DOMAIN = ''
   AUTHOR_API = ''
   WX_API = 'https://open.weixin.qq.com'
-  APP_ID = ''
+  APP_ID = 'wx123456789'
   SNS_SCOPE = 'snsapi_base'
 }
 
